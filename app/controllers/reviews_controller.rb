d9a6c1de61c1d37e8bb357aec4afc1c518f1646c
Tristan Viney <yenivyeniv@gmail.com>
2019-05-15 16:48:41 +0200

    back end complete

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,11 +5,15 @@ class ReviewsController < ApplicationController
   end
 
   def create
-    review = Review.new(review_params)
-    restaurant = Restaurant.find(params[:restaurant_id])
-    review.restaurant = restaurant
-    review.save!
-    redirect_to restaurant_path(restaurant)
+    @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review.restaurant = @restaurant
+
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
   end
 
   private
